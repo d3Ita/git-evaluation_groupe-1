9979bb6b538ab9c748b7217c8c4384d78e0a4615
thibchm <thib.chm@gmail.com>
2026-02-25 08:51:39 +0400

    Add : Gestion des erreurs d'argument non entier

--- generator.py
+++ generator.py
@@ -6,13 +6,18 @@ import random
 
 
 def main():
-    
+    #Erreur : Aucun argument 
     if len(sys.argv) < 2:
         print("Erreur : veuillez fournir le nombre d'expressions à générer.", file=sys.stderr)
         print("Utilisation : ./generator <nombre>", file=sys.stderr)
         sys.exit(1)
 
-    n = int(sys.argv[1])
+    #Erreur : Argument non entier 
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print(f'Erreur : "{sys.argv[1]}" n\'est pas un entier valide.', file=sys.stderr)
+        sys.exit(1)
 
     operations = ['+', '-', '*', '/']
 
